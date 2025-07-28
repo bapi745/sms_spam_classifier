f20b620485580b44b05bd7f7c5c71d39052c8ec4
BAPI LASKAR <166238492+bapi745@users.noreply.github.com>
2025-07-28 22:31:57 +0530

    Update spam_classifier.py
    heatmap is addded

--- spam_classifier.py
+++ spam_classifier.py
@@ -42,6 +42,15 @@ print("Precision", precision_score(y_test, y_pred))
 print("Confusion Matrix\n", confusion_matrix(y_test, y_pred))
 
 
+cm = confusion_matrix(y_test, y_pred)
+plt.figure(figsize=(6, 4))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Ham', 'Spam'], yticklabels=['Ham', 'Spam'])
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix Heatmap')
+plt.show()
+
+
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
